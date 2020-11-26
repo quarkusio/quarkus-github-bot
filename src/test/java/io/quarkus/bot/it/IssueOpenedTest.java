4bafeaa9fe3d8f24d9904e58342d6afd75b0708f
Yoann Rodière <yoann@hibernate.org>
2020-11-26 19:13:04 +0100

    Simple test using quarkiverse-github-app-testing
    Signed-off-by: Yoann Rodière <yoann@hibernate.org>

---
+++ src/test/java/io/quarkus/bot/it/IssueOpenedTest.java
@@ -0,0 +1,37 @@
+package io.quarkus.bot.it;
+
+import io.quarkiverse.githubapp.testing.GitHubAppTestingResource;
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+import org.kohsuke.github.GHEvent;
+
+import java.io.IOException;
+
+import static io.quarkiverse.githubapp.testing.GitHubAppTesting.given;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+@QuarkusTest
+@QuarkusTestResource(GitHubAppTestingResource.class)
+public class IssueOpenedTest {
+
+    @Test
+    void triage() throws IOException {
+        given().github(mocks -> mocks.configFileFromString(
+                "quarkus-bot-java.yml",
+                "triage:\n"
+                + "  rules:\n"
+                + "    - title: test\n"
+                + "      labels: [test1, test2]"
+        ))
+                .when().payloadFromClasspath("/issue-opened.json")
+                .event(GHEvent.ISSUES)
+                .then().github(mocks -> {
+                    verify(mocks.issue(750705278))
+                            .addLabels("test1", "test2");
+                    verifyNoMoreInteractions(mocks.ghObjects());
+                });
+    }
+
+}
