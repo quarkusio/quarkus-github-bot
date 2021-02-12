e859bc6d0e2cefb573e786e9e245c7b76bd10b78
Guillaume Smet <guillaume.smet@gmail.com>
2021-02-12 11:53:31 +0100

    Use a container build to deploy to OpenShift

--- deploy-to-openshift.sh
+++ deploy-to-openshift.sh
@@ -2,6 +2,6 @@
 
 # login to the OpenShift cluster before launching this script
 
-mvn clean package -Dquarkus.kubernetes.deploy=true -Dnative
+mvn clean package -Dquarkus.kubernetes.deploy=true -Dquarkus.native.container-build=true -Dnative
 
 # add kubernetes.io/tls-acme: 'true' to the route to renew the SSL certificate automatically
