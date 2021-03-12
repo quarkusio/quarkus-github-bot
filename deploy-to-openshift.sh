c5c097c8eefd57ea4ec023a15cf01c7950b5a5e1
Guillaume Smet <guillaume.smet@gmail.com>
2021-03-12 11:23:18 +0100

    Add project selection for OpenShift deployment

--- deploy-to-openshift.sh
+++ deploy-to-openshift.sh
@@ -2,6 +2,9 @@
 
 # login to the OpenShift cluster before launching this script
 
+# switch to the right project
+oc project prod-quarkus-bot
+
 mvn clean package -Dquarkus.kubernetes.deploy=true -Dquarkus.native.container-build=true -Dnative
 
 # add kubernetes.io/tls-acme: 'true' to the route to renew the SSL certificate automatically
