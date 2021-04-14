968f3aaa4476a98a2bc3c804ab987a826f12c6ca
Guillaume Smet <guillaume.smet@gmail.com>
2021-04-14 15:07:05 +0200

    Handle more error cases

--- src/main/java/io/quarkus/bot/workflow/report/WorkflowReport.java
+++ src/main/java/io/quarkus/bot/workflow/report/WorkflowReport.java
@@ -3,6 +3,8 @@ package io.quarkus.bot.workflow.report;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.kohsuke.github.GHWorkflowRun.Conclusion;
+
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
 @RegisterForReflection
@@ -11,11 +13,16 @@ public class WorkflowReport {
     private final String sha;
     private final List<WorkflowReportJob> jobs;
     private final boolean sameRepository;
+    private final Conclusion conclusion;
+    private final String workflowRunUrl;
 
-    public WorkflowReport(String sha, List<WorkflowReportJob> jobs, boolean sameRepository) {
+    public WorkflowReport(String sha, List<WorkflowReportJob> jobs, boolean sameRepository, Conclusion conclusion,
+            String workflowRunUrl) {
         this.sha = sha;
         this.jobs = jobs;
         this.sameRepository = sameRepository;
+        this.conclusion = conclusion;
+        this.workflowRunUrl = workflowRunUrl;
     }
 
     public String getSha() {
@@ -54,6 +61,18 @@ public class WorkflowReport {
 
     public boolean isSameRepository() {
         return sameRepository;
+    }
+
+    public boolean isCancelled() {
+        return Conclusion.CANCELLED.equals(conclusion);
+    }
+
+    public boolean isFailure() {
+        return Conclusion.FAILURE.equals(conclusion);
+    }
+
+    public String getWorkflowRunUrl() {
+        return workflowRunUrl;
     }
 
     public boolean hasErrorDownloadingSurefireReports() {
