b2dcb6204446b1af198f740a5d2347d15ab88f51
Guillaume Le Floch <glfloch@gmail.com>
2021-04-23 12:40:26 +0200

    Cancel workflow on pull request close action

--- src/main/java/io/quarkus/bot/workflow/WorkflowConstants.java
+++ src/main/java/io/quarkus/bot/workflow/WorkflowConstants.java
@@ -6,6 +6,7 @@ public class WorkflowConstants {
     public static final String MESSAGE_ID_ACTIVE = "<!-- Quarkus-GitHub-Bot/msg-id:workflow-run-status-active -->";
     public static final String MESSAGE_ID_HIDDEN = "<!-- Quarkus-GitHub-Bot/msg-id:workflow-run-status-hidden -->";
     public static final String QUARKUS_CI_WORKFLOW_NAME = "Quarkus CI";
+    public static final String QUARKUS_DOCUMENTATION_CI_WORKFLOW_NAME = "Quarkus Documentation CI";
     public static final String PULL_REQUEST_NUMBER_PREFIX = "pull-request-number-";
     public static final String JVM_TESTS_PREFIX = "JVM Tests - ";
     public static final String WINDOWS = "Windows";
