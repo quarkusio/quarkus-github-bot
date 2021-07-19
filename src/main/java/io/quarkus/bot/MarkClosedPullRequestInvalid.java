4d3596c2c82024cff12a68fbaa714336eae01d18
Guillaume Smet <guillaume.smet@gmail.com>
2021-07-19 10:54:14 +0200

    Some minor adjustments to MarkClosedPullRequestInvalid and fix the test
    There are interactions with multiples actions so we need to mock things
    a bit more than strictly necessary.

--- src/main/java/io/quarkus/bot/MarkClosedPullRequestInvalid.java
+++ src/main/java/io/quarkus/bot/MarkClosedPullRequestInvalid.java
@@ -26,17 +26,21 @@ class MarkClosedPullRequestInvalid {
         if (pullRequest.isMerged()) {
             return;
         }
+
         if (!quarkusBotConfig.isDryRun()) {
             pullRequest.addLabels(Labels.TRIAGE_INVALID);
-
-            for (GHLabel label : pullRequest.getLabels()) {
-                if (label.getName().startsWith(Labels.TRIAGE_BACKPORT_PREFIX)) {
-                    pullRequest.removeLabel(label.getName());
-                }
-            }
-
         } else {
             LOG.info("Pull request #" + pullRequest.getNumber() + " - Add label: " + Labels.TRIAGE_INVALID);
+        }
+
+        for (GHLabel label : pullRequest.getLabels()) {
+            if (label.getName().startsWith(Labels.TRIAGE_BACKPORT_PREFIX)) {
+                if (!quarkusBotConfig.isDryRun()) {
+                    pullRequest.removeLabel(label.getName());
+                } else {
+                    LOG.info("Pull request #" + pullRequest.getNumber() + " - Remove label: " + label.getName());
+                }
+            }
         }
     }
 }
--- src/test/java/io/quarkus/bot/it/MarkClosedPullRequestInvalidTest.java
+++ src/test/java/io/quarkus/bot/it/MarkClosedPullRequestInvalidTest.java
@@ -1,13 +1,24 @@
 package io.quarkus.bot.it;
 
-import static io.quarkiverse.githubapp.testing.GitHubAppTesting.when;
+import static io.quarkiverse.githubapp.testing.GitHubAppTesting.given;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.withSettings;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.kohsuke.github.GHEvent;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GHWorkflowRun;
+import org.kohsuke.github.GHWorkflowRunQueryBuilder;
+import org.kohsuke.github.PagedIterable;
+import org.kohsuke.github.PagedIterator;
+import org.mockito.Answers;
 
 import io.quarkiverse.githubapp.testing.GitHubAppTestingResource;
 import io.quarkus.bot.util.Labels;
@@ -17,19 +28,39 @@ import io.quarkus.test.junit.QuarkusTest;
 @QuarkusTest
 @QuarkusTestResource(GitHubAppTestingResource.class)
 public class MarkClosedPullRequestInvalidTest {
+
     @Test
     void handleLabels() throws IOException {
-        when().payloadFromClasspath("/pullrequest-closed.json")
+        given().github(mocks -> {
+            // this is necessary because this payload also triggers CancelWorkflowOnClosedPullRequest
+            GHRepository repoMock = mocks.repository("GitHubTestAppRepo");
+            GHWorkflowRunQueryBuilder workflowRunQueryBuilderMock = mock(GHWorkflowRunQueryBuilder.class,
+                    withSettings().defaultAnswer(Answers.RETURNS_SELF));
+            when(repoMock.queryWorkflowRuns())
+                    .thenReturn(workflowRunQueryBuilderMock);
+            PagedIterable<GHWorkflowRun> iterableMock = mockPagedIterable(Collections.emptyList());
+            when(workflowRunQueryBuilderMock.list())
+                    .thenReturn(iterableMock);
+        })
+                .when()
+                .payloadFromClasspath("/pullrequest-closed.json")
                 .event(GHEvent.PULL_REQUEST)
                 .then().github(mocks -> {
                     verify(mocks.pullRequest(691467750))
-                            .addLabels(Labels.TRIAGE_INVALID+"triage/invalid");
+                            .addLabels(Labels.TRIAGE_INVALID);
                     verify(mocks.pullRequest(691467750))
                             .removeLabel(Labels.TRIAGE_BACKPORT_PREFIX);
-                    verifyNoMoreInteractions(mocks.ghObjects());
-
-
                 });
+    }
 
+    @SuppressWarnings("unchecked")
+    private static <T> PagedIterable<T> mockPagedIterable(List<T> contentMocks) {
+        PagedIterable<T> iterableMock = mock(PagedIterable.class);
+        Iterator<T> actualIterator = contentMocks.iterator();
+        PagedIterator<T> iteratorMock = mock(PagedIterator.class);
+        when(iterableMock.iterator()).thenAnswer(ignored -> iteratorMock);
+        when(iteratorMock.next()).thenAnswer(ignored -> actualIterator.next());
+        when(iteratorMock.hasNext()).thenAnswer(ignored -> actualIterator.hasNext());
+        return iterableMock;
     }
 }
