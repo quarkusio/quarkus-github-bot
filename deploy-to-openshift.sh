370b1e8041b0c2903f3a6e29ad8073c1f661f2e1
Guillaume Smet <guillaume.smet@gmail.com>
2021-11-23 19:19:33 +0100

    Choose the project before deleting the image

--- deploy-to-openshift.sh
+++ deploy-to-openshift.sh
@@ -2,11 +2,11 @@
 
 # login to the OpenShift cluster before launching this script
 
-# delete problematic image
-oc delete is ubi-quarkus-native-binary-s2i
-
 # switch to the right project
 oc project prod-quarkus-bot
+
+# delete problematic image
+oc delete is ubi-quarkus-native-binary-s2i
 
 mvn clean package -Dquarkus.kubernetes.deploy=true -Dquarkus.native.container-build=true -Dnative
 
