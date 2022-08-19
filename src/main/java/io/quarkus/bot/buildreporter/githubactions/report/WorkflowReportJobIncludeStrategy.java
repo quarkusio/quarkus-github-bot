7a48cf2d3b6cbc2b1b9054d9c5a1085f9e57a036
Guillaume Smet <guillaume.smet@gmail.com>
2022-08-19 10:36:43 +0200

    Extract the build reporter to a separate project

--- src/main/java/io/quarkus/bot/buildreporter/githubactions/report/WorkflowReportJobIncludeStrategy.java
+++
@@ -1,6 +0,0 @@
-package io.quarkus.bot.buildreporter.githubactions.report;
-
-public interface WorkflowReportJobIncludeStrategy {
-
-    boolean include(WorkflowReport report, WorkflowReportJob job);
-}
--- src/main/java/io/quarkus/bot/buildreporter/githubactions/urlshortener/UrlShortener.java
+++
@@ -1,6 +0,0 @@
-package io.quarkus.bot.buildreporter.githubactions.urlshortener;
-
-public interface UrlShortener {
-
-    String shorten(String url);
-}
