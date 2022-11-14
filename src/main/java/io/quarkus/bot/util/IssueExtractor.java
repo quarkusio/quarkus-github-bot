d665aefb55575d11b593b21079c7c5b6b9945ae5
NicolasGras <nicolasgras01@gmail.com>
2022-11-14 00:38:41 -0500

    Define method to use Github GraphQL Api to extract linked issues of a PR

--- src/main/java/io/quarkus/bot/util/IssueExtractor.java
+++ src/main/java/io/quarkus/bot/util/IssueExtractor.java
@@ -1,9 +1,21 @@
 package io.quarkus.bot.util;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.concurrent.ExecutionException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import javax.json.JsonObject;
+
+import org.kohsuke.github.GHPullRequest;
+import org.kohsuke.github.GHRepository;
+
+import io.smallrye.graphql.client.Response;
+import io.smallrye.graphql.client.dynamic.api.DynamicGraphQLClient;
 
 public class IssueExtractor {
 
@@ -24,5 +36,71 @@ public class IssueExtractor {
             result.add(issueNumber);
         }
         return result;
+    }
+
+    public Set<Integer> extractIssueNumbers(GHPullRequest pullRequest, DynamicGraphQLClient gitHubGraphQLClient) {
+        Set<Integer> result = new TreeSet<>();
+
+        String prBody = pullRequest.getBody();
+        result.addAll(extractFromPRContent(prBody));
+        result.addAll(extractWithGraphQLApi(pullRequest, gitHubGraphQLClient));
+        return result;
+    }
+
+    public Set<Integer> extractFromPRContent(String content) {
+        Set<Integer> result = new TreeSet<>();
+        Matcher matcher = pattern.matcher(content);
+        while (matcher.find()) {
+            Integer issueNumber = Integer.valueOf(matcher.group(1));
+            result.add(issueNumber);
+        }
+        return result;
+    }
+
+    public Set<Integer> extractWithGraphQLApi(GHPullRequest pullRequest, DynamicGraphQLClient gitHubGraphQLClient) {
+
+        GHRepository repository = pullRequest.getRepository();
+        try {
+            Map<String, Object> variables = new HashMap<>();
+            variables.put("owner", repository.getOwnerName());
+            variables.put("repoName", repository.getName());
+            variables.put("prNumber", pullRequest.getNumber());
+
+            Response response = gitHubGraphQLClient.executeSync("""
+                        query($owner: String! $repoName: String! $prNumber: Int!) {
+                            repository(owner: $owner, name: $repoName) {
+                              pullRequest(number: $prNumber) {
+                                closingIssuesReferences(first: 50) {
+                                  edges {
+                                    node {
+                                      number
+                                    }
+                                  }
+                                }
+                              }
+                            }
+                          }
+                    """, variables);
+
+            if (response.hasError()) {
+                throw new IllegalStateException(
+                        "Unable to get closingIssuesReferences for PR #" + pullRequest.getNumber() + " of repository "
+                                + repository.getFullName() + ": " + response.getErrors());
+            }
+
+            return response.getData().getJsonObject("repository")
+                    .getJsonObject("pullRequest")
+                    .getJsonObject("closingIssuesReferences")
+                    .getJsonArray("edges").stream()
+                    .map(JsonObject.class::cast)
+                    .map(obj -> obj.getJsonObject("node").getInt("number"))
+                    .collect(Collectors.toSet());
+
+        } catch (InterruptedException | ExecutionException e) {
+            throw new IllegalStateException(
+                    "Unable to get closingIssuesReferences for PR #" + pullRequest.getNumber() + " of repository "
+                            + repository.getFullName(),
+                    e);
+        }
     }
 }
