a883e9af2176979bbaa1215976477eaf39293520
Guillaume Smet <guillaume.smet@gmail.com>
2025-05-21 16:05:27 +0200

    Do not run tests when deploying

--- deploy-to-openshift.sh
+++ deploy-to-openshift.sh
@@ -8,6 +8,6 @@ oc project prod-quarkus-bot
 # delete problematic image
 oc delete is ubi-quarkus-native-binary-s2i
 
-mvn clean package -Dquarkus.kubernetes.deploy=true -Dquarkus.native.container-build=true -Dnative
+mvn clean package -Dquarkus.kubernetes.deploy=true -Dquarkus.native.container-build=true -Dnative -DskipTests -DskipITs
 
 # add kubernetes.io/tls-acme: 'true' to the route to renew the SSL certificate automatically
